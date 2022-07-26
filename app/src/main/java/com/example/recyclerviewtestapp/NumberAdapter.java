a6f897acb47eb140db508503fb6664d061f064f9
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-26 21:29:58 +0500

    add init adapter

---
+++ app/src/main/java/com/example/recyclerviewtestapp/NumberAdapter.java
@@ -0,0 +1,20 @@
+package com.example.recyclerviewtestapp;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NumberAdapter {
+
+    class NumberViewHolder extends RecyclerView.ViewHolder {
+
+        public NumberViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+
+        }
+    }
+
+}
