4b4478314e695183fa161c653715cb096b0662d8
Ashwaganha-cader <max.cacz@yandex.ru>
2022-07-26 21:01:13 +0500

    add layout manager

--- app/src/main/java/com/example/recyclerviewtestapp/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewtestapp/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.recyclerviewtestapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView numberList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        numberList = findViewById(R.id.rv_list);
+
+        // layoutManager
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        numberList.setLayoutManager(layoutManager);
+
+        numberList.setHasFixedSize(true);
+
+
     }
 }
